e0a5d7a517451c2b6c565f8a7fc2d246a6dfbf3b
Anthony K. <84693877+K-834@users.noreply.github.com>
2024-12-11 19:10:33 -0500

    Update search.js

--- search.js
+++ search.js
@@ -16,12 +16,11 @@ const chatbotResponses = {
 // Respuestas secuenciales
 const sequentialResponses = [
     "En que tipo de herramienta quieres el informe?",
-    "Este es el informe con el power BI, espero que te sirva: LINK"
+    "Este es el informe con el power BI, espero que te sirva:<br><a class='text-blue-600 hover:text-blue-800 font-bold transition duration-300 ease-in-out' href='https://app.powerbi.com/reportEmbed?reportId=e7865d65-cef0-40c5-b1d3-6bad84101da3&appId=1f738850-bcc3-4d63-826f-f6df6f7d86bc&autoAuth=true&ctid=0bd6ed56-c6b6-4949-85da-c5a0cf0f3b6e' target='_blank'> <li> Ver informe </li> </a>",
     // "Puedes buscar información sobre cualquier tema relacionado.",
     // "Si tienes alguna pregunta, no dudes en preguntar.",
     // "Recuerda, siempre estoy aquí para ayudarte, ¡no dudes en preguntar!"
 ];
-
 let currentSequentialIndex = 0; // Controlador de la secuencia de respuestas
 
 // Respuesta predeterminada
@@ -55,15 +54,15 @@ function handleSendMessage() {
 
         if (response) {
             // Si hay una palabra clave, devolver la respuesta personalizada
-            botBubble.textContent = chatbotResponses[response];
+            botBubble.innerHTML = chatbotResponses[response];
         } else {
             // Si no hay palabra clave, responder con la secuencia
             if (currentSequentialIndex < sequentialResponses.length) {
-                botBubble.textContent = sequentialResponses[currentSequentialIndex];
+                botBubble.innerHTML = sequentialResponses[currentSequentialIndex];
                 currentSequentialIndex++; // Incrementar el índice para la próxima respuesta
             } else {
                 // Si ya pasaron todas las respuestas secuenciales, damos una respuesta final
-                botBubble.textContent = defaultResponse;
+                botBubble.innerHTML = defaultResponse;
             }
         }
 
